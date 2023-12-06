679bd98ad93a7e654c7a07458a8a8d5e125d733d
mai43melody <152832493+mai43melody@users.noreply.github.com>
2023-12-06 21:16:48 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,21 @@
+// ローディングアニメーション
+// サイトのデータが全て読み込まれた時の処理。
+// クラスloading_bgがついている要素にクラスactiveを付与。
+window.addEventListener('load', function () {
+    let loadingBg = document.querySelector('.loading_bg');
+    loadingBg.classList.add('active');
 
+    // 5秒後に確認する処理。5秒経っても読み込みが終わっていなかった場合もクラスactiveを付与して画面を表示させる。
+    // 低速回線・エラー対策。
+    setTimeout(function () {
+        if (!loadingBg.classList.contains('active')) {
+            loadingBg.classList.add('active');
+        }
+    }, 5000);
+});
+
+
+// スクロールフェードイン
 const Target = document.querySelectorAll('.fadeIn');
 function observation() {
 
